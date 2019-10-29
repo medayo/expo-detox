efb7f11f0f71906a5d313ff6dbb0bfed4d2eb0cc
Calum MacRae <calummacrae@Calums-MacBook-Pro.local>
2019-10-29 18:22:22 +0000

    Last ../

--- src/replace.ts
+++ src/replace.ts
@@ -2,6 +2,6 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 var basepath = path.join(__dirname + '/../');
-const projectRoot = path.resolve('../../')
+const projectRoot = path.resolve('../../../')
 console.log(projectRoot);
 fs.copyFileSync(basepath + 'replace/AppleSimUtils.js', projectRoot + '/node_modules/detox/src/devices/ios/AppleSimUtils.js');
